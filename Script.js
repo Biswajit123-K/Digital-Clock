b7400fcb58519f1f90c4155aeb16e52d9d29259f
Biswajit123-K <biswajitkumbhakar057@gmail.com>
2025-11-09 14:48:14 +0530

    add style file and changes ui

--- Script.js
+++ Script.js
@@ -1,6 +1,30 @@
 
 const t = document.querySelector(".time-container");
-setInterval(()=>{
-let time = new Date();
-t.textContent = time.toLocaleTimeString();
-})
+const h = document.querySelector(".hour");
+const m = document.querySelector(".minute");
+const s = document.querySelector(".second");
+const present = document.querySelector(".current");
+
+
+setInterval(() => {
+    const time = new Date();
+
+
+    console.log(time.getHours());
+    let hours = time.getHours();
+
+    let formate = (hours >= 12) ? "PM" : "AM";
+    present.textContent = formate;
+
+    let formatehours = (hours > 12) ? hours - 12 : hours;
+
+
+    h.innerText = formatehours;
+
+    m.textContent = time.getMinutes();
+    s.textContent = time.getSeconds();
+
+
+
+
+}, 1000);
